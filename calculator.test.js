9e8476a3618908211cf4ddde08aab4e345652d71
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-19 05:55:18 -0700

    modify file

--- calculator.test.js
+++ calculator.test.js
@@ -2,9 +2,13 @@ const Calculator = require('./calculator');
 
 
 const calc = new Calculator();
-test('2+2 should be 4', ()=> {
-    expect(calc.add(2,2)).toBe(4);
+
+describe ('Calculator - add()', () => {
+    test('2+2 should be 4', ()=> {
+        expect(calc.add(2,2)).toBe(4);
+    });
 });
+
 
 
 test('4*4 should be 16', ()=> {
